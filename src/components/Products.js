86be97c0c377c709cdd9d8c2770357e3cca22de5
kanika.np@acem.edu.in <kanikakumari.vacso@fynd-external.com>
2022-09-06 15:23:08 +0530

    updating changes

--- src/components/Products.js
+++ src/components/Products.js
@@ -37,106 +37,112 @@ const Products = () =>{
             id:6,
             companyName: 'Wings',
             productImage:'/img/product/clothes/img6.png',
-            productName: 'jeans',
-            rating: 3,
-            price: 300
+            productName: 'dress',
+            rating: 4.5,
+            price: 1200
         },
         {
             id:7,
             companyName: 'Wings',
             productImage:'/img/product/clothes/img7.png',
-            productName: 'jeans',
-            rating: 3,
+            productName: 'tshirt',
+            rating: 2,
             price: 300
         },
         {
             id:8,
             companyName: 'Wings',
             productImage:'/img/product/clothes/img8.png',
-            productName: 'jeans',
+            productName: 'dress',
             rating: 3,
-            price: 300
+            price: 3200,
+            discount:30
         },
         {
             id:9,
             companyName: 'Wings',
             productImage:'/img/product/clothes/img9.png',
-            productName: 'jeans',
+            productName: 'sweater',
             rating: 3,
-            price: 300
+            price: 450
         },
         {
             id:10,
             companyName: 'Wings',
             productImage:'/img/product/clothes/img10.png',
-            productName: 'jeans',
+            productName: 'coat',
             rating: 3,
-            price: 300
+            price: 1200,
+            discount:10
         },
         {
             id:11,
             companyName: 'Wings',
             productImage:'/img/product/clothes/img11.png',
-            productName: 'jeans',
-            rating: 3,
-            price: 300
+            productName: 'sweater',
+            rating: 4,
+            price: 600
         },
         {
             id:12,
             companyName: 'Wings',
             productImage:'/img/product/clothes/img12.png',
-            productName: 'jeans',
-            rating: 3,
-            price: 300
+            productName: 'sweater',
+            rating: 5,
+            price: 700,
+            discount:5
         },
         {
             id:13,
             companyName: 'Wings',
             productImage:'/img/product/clothes/img13.png',
-            productName: 'jeans',
-            rating: 3,
-            price: 300
+            productName: 'dress',
+            rating: 5,
+            price: 3200,
+            discount:30
         },
         {
             id:14,
             companyName: 'Wings',
             productImage:'/img/product/clothes/img14.png',
-            productName: 'jeans',
+            productName: 'shirt',
             rating: 3,
-            price: 300
+            price: 700
         },
         {
             id:15,
             companyName: 'Wings',
             productImage:'/img/product/clothes/img15.png',
-            productName: 'jeans',
-            rating: 3,
-            price: 300
+            productName: 'dress',
+            rating: 3.5,
+            price: 4000,
+            discount: 20
         },
        
         {
             id:18,
             companyName: 'Wings',
             productImage:'/img/product/clothes/img18.png',
-            productName: 'jeans',
+            productName: 'trouser',
             rating: 3,
-            price: 300
+            price: 500
         },
         {
             id:19,
             companyName: 'Wings',
             productImage:'/img/product/clothes/img19.png',
-            productName: 'jeans',
+            productName: 'dress',
             rating: 3,
-            price: 300
+            price: 2200
         },
         {
             id:20,
             companyName: 'Wings',
             productImage:'/img/product/clothes/img20.png',
-            productName: 'jeans',
+            productName: 'jump_suit',
             rating: 3,
-            price: 300
+            price: 2300,
+            discount: 20
         }
     ]
     return (<>
@@ -148,16 +154,16 @@ const Products = () =>{
             <img style={{width:"100%"}} src={ele.productImage} alt=""/>
             </div>
             <div className="productDescription">
-            <h1 className="productCompanyName"> Wings</h1>
-                <p className="productName"> Women Jeans</p>
+            <h1 className="productCompanyName"> {ele.companyName}</h1>
+                <p className="productName"> {ele.productName}</p>
 
                 <StarRatingComponent 
                     className="productRating"
                     starCount={5}
-                    value={3}
+                    value={ele.rating}
                 />
 
-                <p className="productPrice"> Rs. 400</p>
+                <p className="productPrice">{ele.price}</p>
             </div>
         </div>
           )
